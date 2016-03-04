87948a0d4cb689b000c2b57545261665c0816b23
LuizHB <lhsbispo@yahoo.com.br>
2016-03-04 01:17:48 -0300

    Create mhd_p2_t1.py
    Second case: a raised to the n power, change n to 2

---
+++ mhd_p2_t1.py
@@ -0,0 +1,14 @@
+
+import sys
+import pylab as py
+import numpy as np
+
+
+def f(x,a):
+
+raR = 12
+  return raR**2 * np.sqrt((np.pow(vth/aw,2)) + 7.3*np.pow(10,-5) * a**2 * raR**2 * np.pow(np.sin(np.pi/2),2)) - ((1600/x)*a
+  
+  
+
+
